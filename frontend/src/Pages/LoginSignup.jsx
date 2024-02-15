a64eb4e3e6334bb655a0fe5d53e99705f9c064c7
legogoUA <olehmedushivskyi@gmail.com>
2024-02-15 22:32:45 +0200

    add category product

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './SCSS/LoginSignup.scss';
 
 export const LoginSignup = () => {
   return (
-    <div></div>
+    <div className='loginsignup'>
+      <div className="loginsignup__container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup__fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup__login">
+          Already have an account? 
+          <span>Login here</span>
+        </p>
+        <div className="loginsignup__agree">
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, I agree to the terms fo use & privacy palicy.</p>
+        </div>
+      </div>
+    </div>
   )
 }
