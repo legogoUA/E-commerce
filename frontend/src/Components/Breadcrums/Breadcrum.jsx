ed3a673cf497879a6cce45a188fe0b12d064fd9f
legogoUA <olehmedushivskyi@gmail.com>
2024-02-20 15:44:44 +0200

    fix link to Product card

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import './Breadcrum.scss';
 import arrow_icon from '../Assets/breadcrum_arrow.png';
 
-export const Breadcrum = (props) => {
+const Breadcrum = (props) => {
   const {product} = props;
 
   return (
     <div className='breadcrum'>
-      HOME <img src={arrow_icon} alt="" /> 
-      SHOP <img src={arrow_icon} alt="" /> 
-      {product.category} <img src={arrow_icon} alt="" /> 
-      {product.name} 
+      HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name} 
     </div>
   )
 }
